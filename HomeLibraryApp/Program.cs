b3e8f8b5579553740b16ddad8118e06867b387e3
Bobby Norris <88409784+bnorris1@users.noreply.github.com>
2023-10-07 14:28:52 -0500

    begin coding UI

---
+++ HomeLibraryApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeLibraryApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int frame = 0;
+            string uiSelection = "- -";
+            
+            while (uiSelection != "8")
+            {
+                Console.WriteLine($"Frame = {frame}");
+                Console.WriteLine($"UI Selection = {uiSelection}\n");
+                UI.PrintWelcomeMessage();
+                UI.PrintHomeMenu();
+                uiSelection = Console.ReadLine();
+
+
+
+
+
+                Console.Clear();
+                frame++;
+            }
+            if (uiSelection == "8")
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ HomeLibraryApp/UI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeLibraryApp
+{
+    static class UI
+    {
+        internal static void PrintWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to your Home Library App\n");
+        }
+        internal static void PrintHomeMenu()
+        {
+            Console.WriteLine("Please select an option:\n" +
+                "1. Initialize Local Library (if none exists)\n" +
+                "2. Load Local Library\n" +
+                "3. List Books\n" +
+                "4. Select a Book (read book)\n" +
+                "5. Create a Book\n" +
+                "6. Cache a Book\n" +
+                "7. Delete a Book\n" +
+                "8. Exit\n");
+        }
+        internal static int ProcessSelection(string input)
+        {
+            
+
+            
+            
+            
+
+
+            return 0;
+        }
+    }
+}
